84a3eea745a40dc8cee7343db44c512afee265ff
Kenny Contreras <kc458@afsconnect1.njit.edu>
2019-09-22 21:33:06 -0400

    updating initDB.php

--- initDB.php
+++ initDB.php
@@ -1,18 +1,48 @@
 <?php
+
 #turn error reporting on
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require('config.php');
-echo $host;
+echo "Loaded host: " . $host;
 
-$connection_string = "mysql:host=$host;dbname=$database;username=$username;password=$password;charset=utf8mb4";
+$connection_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 
 try
 {
 	$db = new PDO($connection_string, $username, $password);
-	echo "\nShould have connected";
+	echo "\nConnected";
+
+	$query = "create table if not exists `TestUsers`(
+		`id` int auto_increment not null,
+        	`username` varchar(30) not null unique,
+        	`pin` int default 0,
+        	PRIMARY KEY(`id`)
+        	) CHARACTER SET utf8 COLLATE utf8_general_ci";
+
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+	$stmt = $db->prepare($query);
+	print_r($stmt->errorInfo());
+
+	$r = $stmt->execute();
+
+	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
+	unset($r);
+
+	$insert_query = "INSERT INTO `TestUsers`(`username`, `pin`) VALUES (:username, :pin)";
+	$stmt = $db->prepare($insert_query);
+	$user = "JohnDoe";
+	$pin = 1234;
+	$r = $stmt->execute(array(":username"=>$user, ":pin"=>$pin));
+
+	echo "<br>" . ($r>0?"Insert successful":"Insert failed") . "<br>";
+	
+	$select_query = "select * from `TestUsers` where username = :username";
+	
+	$result = $stmt->fetch();
+	echo "<br><pre>" . var_export($result, true) . "</pre><br>";
 }
 
 catch(Exception $e)
@@ -21,3 +51,4 @@ catch(Exception $e)
 	exit("\nIt didn't work");
 }
 ?>
+
