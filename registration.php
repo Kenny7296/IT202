9ae992a81f6b76ee3f154b1fcd392ff024c0f283
Kenny Contreras <kc458@afsconnect1.njit.edu>
2019-10-07 13:10:05 -0400

    registration.php

---
+++ registration.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function checkPasswords()
+{
+	if(isset($_POST['password']) && isset($_POST['confirm']))
+	{
+		if($_POST['password'] == $_POST['confirm'])
+		{
+			echo "<br>Passwords Matched!<br>";
+		}
+
+		else
+		{
+			echo "<br>Passwords didn't match!<br>";
+		}
+	}
+}
+?>
+
+<html>
+<head>
+	<script>
+		function validate()
+		{
+			var form = document.forms[0];
+			var password = form.password.value;
+			var conf = form.confirm.value;
+
+			console.log(password);
+			console.log(conf);
+	
+			let pv = document.getElementById("validation.password");
+
+			let succeeded = true;
+	
+			if (password == conf)
+			{
+				pv.style.display = "none";
+			}
+			
+			else	
+			{
+				pv.style.display = "block";
+				pv.innerText = "Passwords don't match";
+				succeeded = false;
+			}
+
+			var email = form.email.value;
+			var ev = document.getElementById("validation.email");
+
+			if(email.indexOf('@') > -1)
+			{
+				ev.style.display = "none";
+			}
+	
+			else
+			{
+				ev.style.display = "block";
+				ev.innerText = "Please enter a valid email address";
+				succeeded = false;
+			}
+			
+			return succeeded;
+                }
+        </script>
+</head>
+<body>
+	<div style="margin-left: 50%; margin-right: 50%;">
+        <form method="POST" action="#" onsubmit="return validate();">
+                <input name="name" type="text" placeholder="Enter your name"/>
+
+		<input name="email" type="email" placeholder="name@example.com"/>
+		<span id="validation.email" style="display:none;"></span>
+
+                <input name="password" type="password" placeholder="Enter a password"/>
+                <input name="confirm" type="password" placeholder="Confirm password"/>
+		<span style="display:none;" id="validation.password"></span>
+
+                <input type="submit" value="Try it"/>
+        </form>
+
+        <br><a href="https://github.com/Kenny7296/IT202/blob/master/myFormAssignment.php">Github Repo</a><br>
+</body>
+</html>
+
+<?php checkPasswords();?>
+<?php
+if(isset($_POST))
+{
+        echo "<br><pre>" . var_export($_POST, true) . "</pre><br>";
+}
+?>
