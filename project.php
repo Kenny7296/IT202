7f6fbf2c1ddf1ca1185ae2abdfe3a28c06e411bc
Kenny Contreras <kc458@afsconnect1.njit.edu>
2019-10-30 16:29:13 -0400

    project

--- project.php
+++ project.php
@@ -7,50 +7,41 @@ error_reporting(E_ALL);
 
 require('config.php'); //pull in config.php so we can access the variables from it
 
-echo "Loaded Host: " . $host;
+//echo "Loaded Host: " . $host;
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 
 try
 {
+	foreach(glob("sql/*.sql") as $filename)
+	{
+		//echo $filename;
+		$sql[$filename] = file_get_contents($filename);
+		//echo $sql[$filename];
+	}
+	
+	ksort($sql);
+	echo "<br><pre>" . var_export($sql, true) . "</pre><br>";
+	
+	//connect to DB
 	$db = new PDO($conn_string, $username, $password);
-	echo "Connected";
 	
-	//create table
-	$query = "create table if not exists `TestUsers`(
-		`id` int auto_increment not null,
-		`username` varchar(30) not null unique,
-		`pin` int default 0,
-		PRIMARY KEY (`id`)
-		) CHARACTER SET utf8 COLLATE utf8_general_ci";
-	
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-	
-	$stmt = $db->prepare($query);
-	print_r($stmt->errorInfo());
-	$r = $stmt->execute();
-	
-	echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
-	
-	unset($r);
-	
-	$insert_query = "INSERT INTO `TestUsers`( `username`, `pin`) 
-		VALUES (:username, :pin)";
-	$stmt = $db->prepare($insert_query);
-	$newUser = "Billy";
-	$newPin = 1234;
-	$r = $stmt->execute(array(":username"=> $newUser, ":pin"=>$newPin));
-	
-	print_r($stmt->errorInfo());
-	
-	echo "<br>" . ($r>0?"Insert successful":"Insert failed") . "<br>";
-	
-	$select_query = "select * from `TestUsers` where username = :username";
-	$stmt = $db->prepare($select_query);
-	$r = $stmt->execute(array(":username"=> "Billy"));
-	$results = $stmt->fetch(PDO::FETCH_ASSOC);
-	
-	//print_r($stmt->errorInfo());
-	echo "<pre>" . var_export($results, true) . "</pre>"; 
+	//$db->setAttribute(PDO::ATTR_ERRMODE);
+	foreach($sql as $key => $value)
+	{
+		echo "<br>Running: " . $key;
+		$stmt = $db->prepare($value);
+		$result = $stmt->execute();
+		$error = $stmt->errorInfo();
+		
+		if($error && $error[0] !== '00000')
+		{
+			echo "<br>Error:<pre>" . var_export($error,true) . "</pre><br>";
+		}
+
+		echo "<br>$key result: " . ($result>0?"Success":"Fail") . "<br>";
+	}
+
+	$db = null;
 }
 
 catch(Exception $e)
@@ -58,24 +49,5 @@ catch(Exception $e)
 	echo $e->getMessage();
 	exit("Something went wrong");
 }
-
 ?>
 
-<html>
-<head>
-</head>
-<body>
-	<form method="POST" action="where/are/we/going.php">
-		<input type="text" name="username" placeholder="Enter your name"/>
-		<input type="password" name="password" placeholder="Enter a password"/>
-		<input type="submit" value="Login"/>
-	</form>
-</body>
-</html>
-
-<?php
-if(isset($_POST))
-{
-	echo "<br><pre>" . var_export($_POST, true) . "</pre><br>";
-}
-?>
---
+++ projectLogin.php
@@ -0,0 +1,58 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//echo "Loaded Host: " . $host;
+if((isset($_POST['name'])) && isset($_POST['password']))
+{		
+	require('config.php');
+	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+
+	$db = new PDO($conn_string, $username, $password);
+	echo "Connected";
+
+	$select_query = "select * from `TestUsers` where username = :username";
+	$stmt = $db->prepare($select_query);
+	$stmt->bindParam(":username", $_POST['username']);
+	$r = $stmt->execute(array(":username"=> "Billy"));
+	$results = $stmt->fetch(PDO::FETCH_ASSOC);
+
+	if($stmt->errorInfo())
+	{
+		print_r($stmt->errorInfo());
+	}
+		
+	else
+	{
+		if($results["password"] == $_POST['password'])
+		{
+			echo $results;
+		}
+			
+		else
+		{
+			//
+		}
+	}
+}	
+?>
+
+<html>
+<head>
+</head>
+<body>
+      	<form method="POST" action="#">
+                <input type="text" name="username" placeholder="Enter username"/>
+                <input type="password" name="password" placeholder="Enter a password"/>
+                <input type="submit" value="Login"/>
+        </form>
+</body>
+</html>
+
+<?php
+if(isset($_POST))
+        {
+                echo "<br><pre>" . var_export($_POST, true) . "</pre><br>";
+        }
+?>
