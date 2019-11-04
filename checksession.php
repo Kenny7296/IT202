a902704db9a8a73b972884ff21b8cf2d5063ff38
Kenny Contreras <kc458@afsconnect1.njit.edu>
2019-11-04 12:39:24 -0500

    sample login + reg

--- checksession.php
+++ checksession.php
@@ -2,6 +2,6 @@
 
 session_start();
 
-echo "We found: " . var_export($_SESSION['loggedInUser']
+echo "We found: " . var_export($_SESSION['loggedInUser'], true);
 
 ?>
--- destroysession.php
+++ destroysession.php
@@ -4,6 +4,7 @@ sessionstart();
 
 session_unset();
 session_destroy();
+
 echo "It's all gone!";
 echo var_export($_SESSION, true);
 
