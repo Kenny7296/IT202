d446df156b5df4ba42f504161522fc8d113ebfd9
Kenny Contreras <kc458@afsconnect1.njit.edu>
2019-11-23 15:30:32 -0500

    ugh

--- FallProject/projectlogin.php
+++ FallProject/projectlogin.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 <html>
 <head></head>
 <body>
-	<form method="POST" onsubmit="return checkPasswords(this);"/>
+	<form method="POST"/>
 		<input type="text" name="username" placeholder="Enter username"/>
 		<input type="password" name="password" placeholder="Enter password"/>
 		<input type="submit" value="Login"/>
@@ -16,13 +16,12 @@ error_reporting(E_ALL);
 </html>
 
 <?php
-
 if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['confirm']))
 {	
 	$user = $_POST['username'];
 	$pass = $_POST['password'];
 	$confirm = $_POST['confirm'];
-		
+
 	try
 	{
 		require('config.php');
@@ -30,25 +29,22 @@ if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['confi
 		$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 		$db = new PDO($conn_string, $username, $password);
 
-		$stmt = $db->prepare("SELECT id, username, password FROM `Users` WHERE username = :username LIMIT 1";
+		$stmt = $db->prepare("SELECT id, username, password FROM `TestUsers` WHERE username = :username LIMIT 1";
 		$stmt->execute(array(":username"=>$user));
 
 		$results = $stmt->fetch(PDO::FETCH_ASSOC);
 	
 		if($results && count($results) > 0)
 		{
-			//$hash = password_hash($pass, PASSWORD_BCRYPT);
 			if(password_verify($pass, $results['password']))
 			{
 				echo "Welcome, " . $results["username"];
 				echo "[" . $results["id"] . "]";
-				$user = array("id"=> $results['id'],
-							"name"=> $results['username']
-								);
-					$_SESSION['user'] = $user;
-					echo var_export($user, true);
-					echo var_export($_SESSION, true);
-					header("Location: samplelandingpage.php");		
+				$user = array("id"=> $results['id'], "name"=> $results['username']);
+				$_SESSION['user'] = $user;
+				echo var_export($user, true);
+				echo var_export($_SESSION, true);
+				header("Location: samplelandingpage.php");		
 			}
 
 			else
@@ -61,7 +57,6 @@ if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['confi
 		{
 			echo "Invalid username";
 		}	
-
 	}
 
 	catch(Exception $e)
