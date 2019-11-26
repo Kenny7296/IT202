3eadcafbd3ee0ac5c0c4de9a7f1b186fec15c9ab
Kenny Contreras <kc458@afsconnect1.njit.edu>
2019-11-25 20:15:46 -0500

    moved files around

--- FallProject/dashboard.php
+++ FallProject/dashboard.php
@@ -1,23 +1,32 @@
+<?php
+session_start();
+?>
+
 <html>
 <head>
-	<style>
-		.nav{padding:1%;}
-	</style>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous">
+</script>
+
+<script>
+	$(document).ready(function()
+	{
+		var nav = ["It works?", "I think so!", "Logout"];
+		let ul = $("<ul>");
+		$("body").append(ul);
+		nav.forEach(function(item, index)
+		{
+			let ele = $("<a>");
+			ele.attr("href", "?page="+item);
+			ele.text(item);
+			ul.append($("<li>").append(ele[0]));
+		});	
+	});
+</script>
 </head>
 <body>
-
-	<?php
-		include_once("partials/header.php");
-		include_once("helpers/functions.php");
-	?>
-
-	<section>Welcome, <?php get_username();?>.</section>
-	<section>
-		<header>Items Due Soon</header>
-		<p>Item one</p>
-		<p>Item two</p>
-		<p>Item three</p>
-		<p>...</p>
-	</section>
+Hello there, <?php echo $_SESSION['user']['name'];?>
 </body>
 </html>
--- FallProject/registration.php
+++ FallProject/registration.php
@@ -1,124 +1,82 @@
-<!DOCTYPE html>
 <?php
-ini_set('display_errors',1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-function checkPasswords()
-{
-	if(isset($_POST['password']) && isset($_POST['confirm']))
-	{
-		if($_POST['password'] == $_POST['confirm'])
-		{
-			echo "<br>Passwords Matched!<br>";
-		}
-
-		else
-		{
-			echo "<br>Passwords didn't match!<br>";
-		}
-	}
-}
 ?>
 
 <html>
 <head>
-	<script>
-		function validate()
+<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+<script>
+$(document).ready(function()
+{
+	$('#register_form').submit(function(event)
+	{
+		if(this.password.value.length == 0 || this.confirm.value.length == 0)
 		{
-			var form = document.forms[0];
-			var password = form.password.value;
-			var conf = form.confirm.value;
+			alert("Please enter a password and confirm it");
+			return false;
+		}
+		
+		let isOk = this.password.value == this.confirm.value;
+		
+		if(!isOk)
+		{
+			alert("Password and Confirm password don't match");
+		}
 
-			console.log(password);
-			console.log(conf);
-	
-			let pv = document.getElementById("validation.password");
-
-			let succeeded = true;
-	
-			if (password == conf)
-			{
-				pv.style.display = "none";
-				form.confirm.className = "noerror";
-			}
-			
-			else	
-			{
-				pv.style.display = "block";
-				pv.innerText = "Passwords don't match";
-				//form.confirm.focus();
-				form.confirm.className = "error";
-				//form.confirm.style = "border: 1px solid red;";
-				succeeded = false;
-			}
-
-			var email = form.email.value;
-			var ev = document.getElementById("validation.email");
-
-			if(email.indexOf('@') > -1)
-			{
-				ev.style.display = "none";
-			}
-	
-			else
-			{
-				ev.style.display = "block";
-				ev.innerText = "Please enter a valid email address";
-				succeeded = false;
-			}
-
-			var sel = form.dd;
-
-			if (sel.selectedIndex == 0)
-			{
-				alert("Please pick a value");
-				succeeded = false;
-			}
-
-			console.log(sel.options[sel.selectedIndex].value);
-
-			return succeeded;
-                }
-        </script>
-	<style>
-		input { border: 1px solid black; }
-		.error {border: 1px solid red;}
-		.noerror {border: 1px solid black;}
-	</style>
+		return isOk;
+	});
+});
+</script>
 </head>
 <body>
-	<div style="margin-left: 50%; margin-right: 50%;">
-        <form method="POST" action="#" onsubmit="return validate();">
-		
-		<input name="name" type="text" placeholder="Enter your name"/>
-
-		<input name="email" type="email" placeholder="name@example.com"/>
-		<span id="validation.email" style="display:none;"></span>
-		
-
-                <input name="password" type="password" placeholder="Enter a password"/>
-                <input name="confirm" type="password" placeholder="Confirm password"/>
-		<span id="validation.password" style="display:none;"></span>
-
-		<select name="dd" class="required">
-			<option value="-1">Select One</option>
-			<option value="0">This one?</option>
-			<option value="1">No, this one!</option>
-			<option value="2">Can't be this One.</option>
-		</select>
-
-		<input type="submit" value="Try it"/>
-        </form>
-
-        <br><a href="https://github.com/Kenny7296/IT202/blob/master/registration.php">Github Repo</a><br>
+	<form id="register_form" method="POST"/>
+		<input type="text" name="username" placeholder="Enter username"/>
+		<input type="password" name="password" placeholder="Enter password"/>
+		<input type="password" name="confirm" placeholder="Confirm password"/>
+		<input type="submit" value="Register"/>
+	</form>
 </body>
 </html>
 
-<?php checkPasswords();?>
 <?php
-if(isset($_POST))
-{
-        echo "<br><pre>" . var_export($_POST, true) . "</pre><br>";
-}
+	if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['confirm']))
+	{		
+		$user = $_POST['username'];
+		$pass = $_POST['password'];
+		$confirm = $_POST['confirm'];
+		
+		if($pass != $confirm)
+		{
+			echo "Passwords don't match";
+			exit();
+		}
+
+		try
+		{
+			$hash = password_hash($pass, PASSWORD_BCRYPT);
+
+			require("config.php");
+
+			$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+			$db = new PDO($conn_string, $username, $password);
+			
+			$stmt = $db->prepare("INSERT INTO `Users` (`username`, `password`) VALUES(:username, :password)");
+			$result = $stmt->execute(array(":username"=>$user, ":password"=>$hash));
+
+			print_r($stmt->errorInfo());
+			
+			echo var_export($result, true);
+			header("Location: login.php");
+		}
+
+		catch(Exception $e)
+		{
+			echo $e->getMessage();
+		}
+	}
 ?>
