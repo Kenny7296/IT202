d1ff57d3d0e9978c548c35d769b64fc9d2905902
Kenny Contreras <kc458@afsconnect1.njit.edu>
2019-11-25 20:42:57 -0500

    added process.php

--- FallProject/dashboard.php
+++ FallProject/dashboard.php
@@ -30,3 +30,70 @@ session_start();
 Hello there, <?php echo $_SESSION['user']['name'];?>
 </body>
 </html>
+
+<?php
+	require ("configure.php");
+	
+	if (isset($_GET['h1']))
+	{
+		$qID = $_GET['h1'];
+	}
+	
+	else
+	{
+		$qID = 1;
+	}
+
+	$question = 'Question not set';
+	$answerA = 'unchecked';
+	$answerB = 'unchecked';
+	$answerC = 'unchecked';
+
+	$A = "";
+	$B = "";
+	$C = "";
+
+	$conn_string = "survey";
+
+	$db = new PDO($conn_string, $username, $password);
+
+	if ($db)
+	{
+		$stmt = $db->prepare("SELECT ID, Question, OptionA, OptionB, OptionC FROM Questions WHERE ID = ?");
+
+		if ($stmt)
+		{
+			$stmt->bind_param('i', $qID);
+			$stmt->execute();
+			$res = $stmt->get_result();
+
+			if ($res->num_rows > 0)
+			{
+				$row = $res->fetch_assoc();
+
+				$qID = $row['ID'];
+				$question = $row['Question'];
+				$A = $row['OptionA'];
+				$B = $row['OptionB'];
+				$C = $row['OptionC'];
+
+			}
+
+			else
+			{
+				print "Error - No rows";
+			}
+		}
+
+		else
+		{
+			print "Error - DB ERROR";
+		}
+
+	}
+	else {
+		print "Error getting Survey";
+	}
+
+
+?>
---
+++ FallProject/process.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+require 'config.php';
+$voteMessage = "";
+
+if((isset($_SESSION['Voted'])))
+{
+	if ($_SESSION['Voted'] = '1')
+	{
+		$voteMessage = "You've already voted...";
+	}
+}
+
+else
+{
+	if(isset($_GET['Submit1']) && isset($_GET['q']))
+	{
+		$selected_radio = $_GET['q'];
+		$IDnumber = $_GET['h1'];
+
+		$conn_string = "survey";
+
+		$db = new PDO($conn_string, $username, $password);
+
+		if($db)
+		{
+			if($selected_radio == "A")
+			{
+				$vote = "UPDATE Questions SET VotedA = VotedA + 1 WHERE ID = ?";
+				$voteMessage = insert_vote($db, $vote, $IDnumber);
+			}
+
+			else if($selected_radio == "B")
+			{
+				$vote = "UPDATE Questions SET VotedB = VotedB + 1 WHERE ID = ?";
+				$voteMessage = insert_vote($db, $vote, $IDnumber);
+			}
+			
+			else
+			{
+				print "Error - could not record vote";
+			}	
+		}
+	}
+	
+	else
+	{
+		print "Please vote!";
+	}
+}
+
+function insert_vote($db, $sql, $id)
+{
+	$stmt = $db->prepare($sql);
+	$stmt->bind_param('i', $id);
+	$stmt->execute();
+
+	return "Thanks for voting!";
+}
+
+?>
+
+<html>
+<head>
+</head>
+<body>
+<?PHP print $voteMessage . "<BR>"; ?>
+</body>
+</html>
