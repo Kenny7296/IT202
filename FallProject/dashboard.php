10582e02c84e86010c13a8852d7fb0414a3aa48c
Kenny Contreras <kc458@afsconnect1.njit.edu>
2019-12-09 11:29:58 -0500

    moved files around

--- FallProject/dashboard.php
+++ FallProject/dashboard.php
@@ -1,8 +1,55 @@
 <?php
-session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+function view_item($ID)
+{
+	require("config.php");
+	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+	$db = new PDO($conn_string, $username, $password);
+	
+	$stmt = $db->prepare("SELECT * FROM `Questions` WHERE ID = :ID");
+	$stmt->execute(array(":ID"=>$ID));
+	$results = $stmt->fetch(PDO::FETCH_ASSOC);
+	return $results;
+}
+
+function update_item($ID, $A, $B, $choice)
+{
+	$A = "";
+        $B = "";
+
+        if($choice == "A")
+	{
+		$A++;
+	}
+
+	if($choice == "B")
+	{
+		$B++;
+	}
+
+	require("config.php");
+	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+	$db = new PDO($conn_string, $username, $password);
+
+	$stmt = $db->prepare("UPDATE `Questions` SET VotedA = VotedA + :VotedA, VotedB = VotedB + :VotedB, WHERE ID = :ID");
+	$r = $stmt->execute(array(":ID"=> $ID, "VotedA + :VotedA"=> $A, "VotedB + :VotedB"=> $B));
+
+	return $r > 0;
+}
+?>
+
+<?php
+	if(isset($_POST['ID']))
+	{
+		update_item($_POST['ID'], $_POST['VotedA'], $_POST['VotedB']);
+	}
+?>
+
+<?php
+	$ID = 1;
+	if(isset($_POST['choice']))
+	{
+		$ID = $_POST['choice'];
+	}
 ?>
 
 <html>
@@ -19,7 +66,6 @@ error_reporting(E_ALL);
 		var nav = ["It works?", "I think so!", "Logout"];
 		let ul = $("<ul>");
 		$("body").append(ul);
-
 		nav.forEach(function(item, index)
 		{
 			let ele = $("<a>");
@@ -31,57 +77,18 @@ error_reporting(E_ALL);
 </script>
 </head>
 <body>
-	Hello there, <?php echo $_SESSION['user']['name'];?>
-
-	<form name="form1" method="POST">
-		<input type="hidden" name="ID" value="<?php echo $row['ID'];?>"/>
-		<input type="radio" name="A" value="<?php echo $row['OptionA'];?>"/>
-		<input type="radio" name="B" value="<?php echo $row['OptionB'];?>"/> 
-
-		<input type="Submit" value="Click here to vote"/>
-	</form>
-
-
-	<form name="form2" method="POST" action="results.php">
-		<input type="Submit" name="Submit2" value="View results"/>
-	</form>
+<?php $row = view_item($ID);?>
+<?php if($row): ?>
+	<article>
+		<p><?php echo $row['Question']; ?></p>
+		<form method="POST">
+			<label for="yes">Yes</label>
+			<input type="radio" name="choice" id="yes" value="A" <?php echo $row['OptionA'];?>/>
+			<label for="no">No</label>
+			<input type="radio" name="choice" id="no" value="B" <?php echo $row['OptionB'];?>/>
+			<input type="Submit" value="Pick"/>
+		</form>
+	</article>
+<?php endif; ?>
 </body>
-0</html>
-
-<?php
-	if(isset($_POST['Question']))
-	{
-		$Question = $_POST['Question'];
-
-		try
-		{
-			require ("config.php");
-
-			$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
-			$db = new PDO($conn_string, $username, $password);
-
-			$stmt = $db->prepare("SELECT ID, Question, OptionA, OptionB FROM `Questions` WHERE Question = :Question");
-			$stmt->execute(array(":Question"=>$Question));
-
-			$results = $stmt->fetch(PDO::FETCH_ASSOC);
-
-			if($results && count($results) > 0)
-			{
-				$Question = array("ID"=> $results['ID'], "question"=> $results['Question'], "A"=> $results['OptionA'], "B"=> $results['OptionB']);
-				$_SESSION['Question'] = $Question;
-				echo var_export($Question, true);
-				//echo var_export($_SESSION, true);
-			}
-		
-			else
-			{
-				echo "Error getting survey";
-			}
-		}
-
-		catch(Exception $e)
-		{
-			echo $e->getMessage();
-		}
-	}
-?>
+</html>
